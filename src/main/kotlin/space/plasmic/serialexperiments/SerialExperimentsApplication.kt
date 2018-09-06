138e875a7322cc804f0896a0039b3f266497fc38
toming <tomingtoming@gmail.com>
2018-09-06 21:35:26 +0900

    Spring Initializr: MVC and JPA

---
+++ src/main/kotlin/space/plasmic/serialexperiments/SerialExperimentsApplication.kt
@@ -0,0 +1,11 @@
+package space.plasmic.serialexperiments
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SerialExperimentsApplication
+
+fun main(args: Array<String>) {
+    runApplication<SerialExperimentsApplication>(*args)
+}
